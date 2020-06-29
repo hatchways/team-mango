9004a01a4a9dae7ae250456f654aeda6c220ecf2
Albert <abohakobyan@hotmail.com>
2020-06-29 13:24:33 -0400

    account route added

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
+const accountRouter = require("./routes/account");
+
 
 const { json, urlencoded } = express;
 
@@ -19,6 +21,9 @@ app.use(express.static(join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/ping", pingRouter);
+app.use("/", accountRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
