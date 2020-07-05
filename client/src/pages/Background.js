f5692b8906b76559c579308fe1944054b3ba4480
Charm Sum Yu <samsoncsyu77771402@gmail.com>
2020-07-05 07:58:25 -0400

    comments in Backgound.js

--- client/src/pages/Background.js
+++ client/src/pages/Background.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; //useState: one of Hooks in React to store states
 
 import {
   Typography,
@@ -12,18 +12,17 @@ import {
   Select,
   InputLabel,
   MenuItem,
-} from "@material-ui/core";
+} from "@material-ui/core"; //Styled components from Material-UI/core
 import MuiAlert from "@material-ui/lab/Alert";
-import Rating from '@material-ui/lab/Rating';
-import { withStyles } from "@material-ui/core/styles";
-import { Route, Link } from "react-router-dom";
-import pic1 from "../assets/pic1.png";
+import Rating from '@material-ui/lab/Rating'; //Some components from Material-UI/lab
+import { withStyles } from "@material-ui/core/styles"; //Hook from Material-UI for styles
+import { Route, Link } from "react-router-dom"; //components for routing to other pages
 
-function Alert(props) {
+function Alert(props) { //MuiAlert is a standard original component from Material-UI. Alert is a styled one with following properties
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-const backgroundStyle = (theme) => ({
+const backgroundStyle = (theme) => ({//styling for this page. It will be used with withStyles at the end of the file
   root: {
     flexGrow: 1,
     height: "100vh",
@@ -66,14 +65,14 @@ const backgroundStyle = (theme) => ({
     margin: "2rem 0 .5rem 0",
   }
 });
-const labels = {
+const labels = { //array for Rating component
   1: "JUNIOR",
   2: "ADVANCED JUNIOR",
   3: "INTERMEDIATE",
   4: "SENIOR",
   5: "ADVANCED SENIOR",
 }
-const details = {
+const details = { //array for Rating component
   1: "Do not have any experience on interviews",
   2: "Had one or two interviews before",
   3: "Had a few job interviews, but need more practice",
@@ -81,47 +80,47 @@ const details = {
   5: "Had more than twelve interviews before, and know much about interview skills",
 }
 
-function Background(props){
+function Background(props){ //the function component for the background page is declared here
 
-  const [openSnack, setOpenSnack] = useState(false);
+  const [openSnack, setOpenSnack] = useState(false); //openSnack is one of the states. setOpenSnack is its method to change it. False is default value.
   const [severity, setSeverity] = useState('error');
   const [language, setLanguage] = useState('React');
   const [experience, setExperience] = useState('');
   const [value, setValue] = useState(2);
   const [hover, setHover] = useState(-1);
  
-  const handleClick = () => {
+  const handleClick = () => { //the method to handle "next step" button being clicked
     setOpenSnack((experience === "" ) ? true : false );
   }; 
-  const handleClose = (event, reason) => {
+  const handleClose = (event, reason) => { //the method to handle Snack "x" button being clicked
     if (reason === "clickaway") {
       return;
     }
     setOpenSnack(false);
   };
-  const languageChange = (e) => {
+  const languageChange = (e) => { //change the value(state) of language when it is selected
     setLanguage(e.target.value);
   };
-  const experienceChange = (e) => {
+  const experienceChange = (e) => { //change the value(state) of experience when it is selected
     setExperience(e.target.value);
   };
 
-    const { classes } = props;
+    const { classes } = props; //classes property for styling is passed at the last line of this file
     return (
-      <div className={classes.root}>
-        <Paper className={classes.paper} elevation={0}>
+      <div className={classes.root}> {/*use classNmae instead of class in React. div is a HTML tag */}
+        <Paper className={classes.paper} elevation={0}> {/*Paper and Grid are Material-UI components*/}
           <Grid container spacing={0}>
             <Grid item>
-              <Grid className={classes.image}></Grid>
-            </Grid>
+              <Grid className={classes.image}></Grid> {/*root, paper, image, ... are styled above*/}
+            </Grid> {/*Grid is usually used for the format of the layout*/}
             <Grid className={classes.text} item xs={12} sm container>
               <Grid item xs container direction="column" spacing={0}>
                 <Grid item xs>
-                  <Typography
+                  <Typography 
                     className={classes.title}
                     align="center"
                     variant="h2"
-                  >
+                  >{/*styled component for text*/}
                     Tell about your background
                   </Typography>
                   <Grid>
@@ -135,7 +134,7 @@ function Background(props){
                         id="demo-simple-select-outlined"
                         value={language}
                         onChange={languageChange}
-                      >
+                      > {/*Select component with five options. I will use "map" and array instead of following five lines*/}
                         <MenuItem value={"React"}>React</MenuItem>
                         <MenuItem value={"JavaScript"}>JavaScript</MenuItem>
                         <MenuItem value={"Node.js"}>Node.js</MenuItem>
@@ -178,7 +177,7 @@ function Background(props){
                           setHover(newHover);
                         }}
                       />
-                      {value !== null
+                      {value !== null //conditional render: only display this div when value !== null
                         &&
                         <div>
                         <Box ml={0} style={{color: "#0000ff"}}>
@@ -196,7 +195,7 @@ function Background(props){
                       className={classes.nextStep}
                       onClick={handleClick}
                       color = "primary"
-                    >
+                    > {/*use {} for state and method instead of ""*/}
                       NEXT STEP
                     </Button>
                     <Snackbar
