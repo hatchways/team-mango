d217a3ea99cd42cb219581890464a00beaca5355
Albert <abohakobyan@hotmail.com>
2020-07-07 18:15:38 -0400

    Make code run

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Redirect } from "react-router";
-import PrivateRoute from "./PrivateRoute";
 
 import { theme } from "./themes/theme";
 import SignUp from "./pages/SignUp";
-import Background from "./pages/Background";
 
 import "./App.css";
 
 function App(props) {
-
   return (
-      <MuiThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Route exact path="/" render={() => <Redirect to="/signup" />} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/login" component={SignUp} />
-          <PrivateRoute path="/background" component={Background} />
-        </BrowserRouter>
-      </MuiThemeProvider>
+    <MuiThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Route exact path="/" render={() => <Redirect to="/signup" />} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={SignUp} />
+      </BrowserRouter>
+    </MuiThemeProvider>
   );
 }
 
