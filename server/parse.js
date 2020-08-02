9bc9f32d7b7acfe70596416fe0f3f4b938c6feeb
Albert <abohakobyan@hotmail.com>
2020-08-02 14:49:32 -0400

    fixed parsing questions with better async logic

--- server/parse.js
+++ server/parse.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-fs = require("fs");
+fs = require("fs").promises;
 
 mongoose.connect(process.env.MONGODB_LOCAL_CONNECTION_STRING, {
   useNewUrlParser: true,
@@ -10,79 +10,79 @@ mongoose.connect(process.env.MONGODB_LOCAL_CONNECTION_STRING, {
 });
 
 const Question = require("./question/question.model");
-const { title } = require("process");
-const { time } = require("console");
-
-let datastring = async function () {
-  fs.readFile("./out.html", "utf8", async function (err, data) {
-    if (err) {
-      throw err;
-    }
-    const res = await parse(data).catch((err) => console.log(err));
-    return res;
-  });
-};
-async function parse(data) {
-  const difficultyHard = ["Advanced", "Expert"];
-  let datarr = data.split("<hr>");
-  let resArray = await datarr.forEach(async (element) => {
-    let titleTemp = element.match(
-      /\*+\n*<div id="title">\n*.+\n*<\/div>\n*\*+/
-    );
-    let dif = element.match(/@+\n*<div id="dif">\n*.+\n*<\/div>\n*@+/);
-    if (titleTemp && dif) {
-      titleTemp = titleTemp.toString();
-      dif = dif.toString();
-      titleTemp = titleTemp.replace(/\*+\n*<div id="title">\n*/, "");
-      dif = dif.replace(/@+\n*<div id="dif">\n*/, "");
-      let title = titleTemp.replace(/\n*<\/div>\n*\*+/, "");
-      let difNum = dif.replace(/\n*<\/div>\n*@+/, "");
-      difNum = difNum.trim();
-      desc = element.replace(/\*+\n*<div id="title">\n*.+\n*<\/div>\n*\*+/, "");
-      desc = desc.replace(/@+\n*<div id="dif">\n*.+\n*<\/div>\n*@+/, "");
-      if (title && desc && difNum) {
-        console.log(difNum);
-        let difficulty = " ";
-        if (difNum == "1") {
-          difficulty = "Beginner";
-        } else if (difNum == "2") {
-          difficulty = "Intermediate";
-        } else {
-          difficulty =
-            difficultyHard[Math.floor(Math.random() * difficultyHard.length)];
-        }
-
-        let res = new Question({
-          title: title,
-          description: desc,
-          difficulty: difficulty,
-          random: Math.random(),
-        });
-
-        [res] = await Promise.all([res.save()]);
-
-        return res;
-      }
-    }
-  });
-
-  return resArray;
-}
-
+const fileToParse = "./out.html";
 const connection = mongoose.connection;
-connection.once("open", () => {
-  connection.db.listCollections().toArray((err, collections) => {
-    console.log("Connected to mongoDB");
-    collections.forEach((item) => {
-      if (item.name === "questions") {
-        connection.db.dropCollection("questions");
-        console.log("Dropped documents in collection 'questions'");
+
+//Reads the file and calls sendData for each question
+let datastring = async function () {
+  const data = await fs.readFile(fileToParse, "utf8");
+  let datarr = await data.split("<hr>");
+  return Promise.all(datarr.map((item) => sendData(item)));
+};
+//Reads and seperates Title, Difficulty and description and saves it to DB
+const sendData = async (element) => {
+  const difficultyHard = ["Advanced", "Expert"];
+
+  let titleTemp = await element.match(
+    /\*+\r*\n*<div id="title">\r*\n*.+\r*\n*<\/div>\r*\n*\*+/
+  );
+
+  let dif = await element.match(
+    /@+\r*\n*<div id="dif">\r*\n*.+\r*\n*<\/div>\r*\n*@+/
+  );
+
+  if (titleTemp && dif) {
+    titleTemp = await titleTemp.toString();
+    dif = await dif.toString();
+    titleTemp = await titleTemp.replace(/\*+\r*\n*<div id="title">\r*\n*/, "");
+    dif = await dif.replace(/@+\r*\n*<div id="dif">\r*\n*/, "");
+    let title = await titleTemp.replace(/\r*\n*<\/div>\r*\n*\*+/, "");
+    let difNum = await dif.replace(/\r*\n*<\/div>\r*\n*@+/, "");
+    difNum = await difNum.trim();
+    desc = await element.replace(
+      /\*+\r*\n*<div id="title">\r*\n*.+\r*\n*<\/div>\r*\n*\*+/,
+      ""
+    );
+    desc = await desc.replace(
+      /@+\r*\n*<div id="dif">\r*\n*.+\r*\n*<\/div>\r*\n*@+/,
+      ""
+    );
+    if (title && desc && difNum) {
+      let difficulty = " ";
+      if (difNum == "1") {
+        difficulty = "Beginner";
+      } else if (difNum == "2") {
+        difficulty = "Intermediate";
+      } else {
+        difficulty =
+          difficultyHard[Math.floor(Math.random() * difficultyHard.length)];
       }
-    });
+
+      let res = new Question({
+        title: title,
+        description: desc,
+        difficulty: difficulty,
+        random: Math.random(),
+      });
+
+      await Promise.all([res.save()]);
+      return Promise.resolve("Saved Question");
+    }
+  }
+};
+
+connection.once("open", () => {
+  connection.db.listCollections().toArray(async (err, collections) => {
+    console.log("Connected to mongoDB");
+    await Question.deleteMany({});
+    console.log("removed all the questions");
+
     datastring()
       .then((res) => {
-        console.log(res);
-        //Closing connection breaks the code
+        console.log(
+          "Successfully added questions to the database from " + fileToParse
+        );
+        connection.close();
       })
       .catch((err) => console.log(err));
   });
